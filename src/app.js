d1609e0b78b44c03f75a9acb5464a7f73c9ccc3b
Eunicewangombe <163239656+Eunicewangombe@users.noreply.github.com>
2025-03-07 12:33:17 -0800

    injected more weather data

--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
 function updateWeather(response){
     let temperatureElement=document.querySelector("#temperature");
     temperatureElement.innerHTML=response.data.temperature.current;
+    let descriptionElement=document.querySelector("#description");
+    descriptionElement.innerHTML=response.data.condition.description;
+    let humidityElement=document.querySelector("#humidity");
+    humidityElement.innerHTML=`${response.data.temperature.humidity}% `;
+    let windElement=document.querySelector("#wind-speed");
+    windElement.innerHTML=`${response.data.wind.speed}Km/h `;
+    let timeElement=document.querySelector("#time");
+    let date=new Date(response.data.time* 1000);
+    timeElement.innerHTML=formatDate(date);
 }
-
+function formatDate(date){
+    let minutes=date.getMinutes();
+    let hours=date.getHours();
+    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    let day=days[date.getDay()];
+    if (minutes<10){
+        minutes=`0${minutes}`;
+        
+    }
+    return `${day} ${hours}:${minutes}`;
+    
+}
 function searchCity(city){
     //make API call and update the iterface
     let apiKey="40394c97bf43843e5a265de718ofb8t4";
@@ -17,6 +37,7 @@ function handleSearchSubmit(event){
 let searchInput=document.querySelector("#search-form-input");
 let cityElement=document.querySelector("#city");
 cityElement.innerHTML= searchInput.value;
+
 searchCity(searchInput.value);
 }
 
